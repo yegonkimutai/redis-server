a606963cdfeb8a7e0dbe5d7129c924c3650c7f4c
Yegon <yegonkimutai.99@gmail.com>
2024-06-12 12:06:52 +0300

    Establish redis connection

--- app/main.js
+++ app/main.js
@@ -9,7 +9,38 @@ const handleCommand = (command) => {
     } else if (cmd === 'ECHO' && args.length > 1) {
       const message = args.slice(1).join(' ');
       return `$${message.length}\r\n${message}\r\n`;
-    } else {
+    } else if (cmd === 'SET' && args.length >= 3) {
+        const key = args[1];
+    const value = args[2];
+    store[key] = value;
+
+    if (args.length > 3 && args[3].toUpperCase() === 'PX' && !isNaN(args[4])) {
+      const expiryTime = parseInt(args[4], 10);
+      const expiryTimestamp = Date.now() + expiryTime;
+
+      if (expiryTimes[key]) {
+        clearTimeout(expiryTimes[key].timeoutId);
+      }
+
+      expiryTimes[key] = {
+        expiryTimestamp,
+        timeoutId: setTimeout(() => {
+          delete store[key];
+          delete expiryTimes[key];
+        }, expiryTime)
+      };
+    }
+
+    return '+OK\r\n';
+      } else if (cmd === 'GET' && args.length > 1) {
+        const key = args[1];
+        if (key in store) {
+          const value = store[key];
+          return `$${value.length}\r\n${value}\r\n`;
+        } else {
+          return '$-1\r\n';
+        }
+      }else {
       return '-ERR unknown command\r\n';
     }
   };
@@ -18,7 +49,6 @@ const handleCommand = (command) => {
 const server = net.createServer((connection) => {
   // Handle connection
   connection.on('data', (data) => {
-    console.log(`Received data: ${data}`);
     const response = handleCommand(data.toString());
     connection.write(response);
   });
